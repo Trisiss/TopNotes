361b8f4ba7639f4bca4c493498eb6cd0f5973b53
Trisiss <trisiss@yandex.ru>
2020-08-16 00:20:44 +0700

    Add RecyclerView

---
+++ app/src/main/java/ru/trisiss/topnotes/adapters/ListNotesAdapter.kt
@@ -0,0 +1,46 @@
+package ru.trisiss.topnotes.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+import ru.trisiss.data.models.Note
+import ru.trisiss.topnotes.BR
+import ru.trisiss.topnotes.databinding.ViewNoteBinding
+import ru.trisiss.topnotes.viewmodels.ListNotesViewModel
+
+
+class ListNotesAdapter() :
+    RecyclerView.Adapter<ListNotesAdapter.ListNotesViewHolder>() {
+    var notes: List<Note> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListNotesViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+//        val binding =
+//            DataBindingUtil.inflate<ViewDataBinding>(layoutInflater, viewType, parent, false)
+
+        return ListNotesViewHolder(ViewNoteBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+//        return ListNotesViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return notes.size
+    }
+
+    override fun onBindViewHolder(holder: ListNotesViewHolder, position: Int) {
+        val viewModel: ListNotesViewModel = ListNotesViewModel()
+        holder.bind(viewModel = viewModel, position = position)
+    }
+
+    class ListNotesViewHolder(val binding: ViewDataBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(viewModel: ListNotesViewModel, position: Int) {
+//            binding.setVariable(BR.viewmodel, viewModel)
+//            binding.setVariable(BR.position, position)
+//            binding.executePendingBindings()
+        }
+    }
+
+}
--- app/src/main/java/ru/trisiss/topnotes/fragments/ListNotesFragment.kt
+++ app/src/main/java/ru/trisiss/topnotes/fragments/ListNotesFragment.kt
@@ -1,33 +1,51 @@
 package ru.trisiss.topnotes.fragments
 
-import androidx.lifecycle.ViewModelProviders
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.get
+import ru.trisiss.data.models.Note
 import ru.trisiss.topnotes.viewmodels.ListNotesViewModel
 import ru.trisiss.topnotes.R
+import ru.trisiss.topnotes.adapters.ListNotesAdapter
+import ru.trisiss.topnotes.databinding.FragmentNotesListBinding
 
 class ListNotesFragment : Fragment() {
 
     companion object {
         fun newInstance() = ListNotesFragment()
     }
-
-    private lateinit var viewModel: ListNotesViewModel
+    private lateinit var binding: FragmentNotesListBinding
+    private val viewModel: ListNotesViewModel by lazy { ViewModelProviders.of(this).get(ListNotesViewModel::class.java) }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_notes_list, container, false)
+        binding = FragmentNotesListBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.viewmodel = viewModel
+        binding.rvListNotes.adapter = viewModel.adapter
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(ListNotesViewModel::class.java)
+//        viewModel = ViewModelProvider(this).get(ListNotesViewModel::class.java)
+        val listNotes = listOf<Note>(Note(id = 1, title = "First note", text = "First note", dateModification = "2020-07-27"),
+            Note(id = 1, title = "First note", text = "First note", dateModification = "2020-07-27"),
+            Note(id = 1, title = "First note", text = "First note", dateModification = "2020-07-27"),
+            Note(id = 1, title = "First note", text = "First note", dateModification = "2020-07-27"),
+            Note(id = 1, title = "First note", text = "First note", dateModification = "2020-07-27"))
+        viewModel.setNotesInAdapter(listNotes)
         // TODO: Use the ViewModel
+
     }
 
 }
